9c2392df5731defefe3c9ee3765b0ae440c043dc
Marchino1978 <marco.brambill@gmail.com>
2026-08-21 22:36:09 +0200

    fix

---
+++ esp32/config.example.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Enter your credentials here and rename this file to config.h
+// NOTE: if you only have 1 or 2 Wi-Fi networks, just enter the same credentials in the remaining fields.
+const char* ssid_home    = "YOUR_HOME_SSID";
+const char* pass_home    = "YOUR_HOME_PASSWORD";
+
+const char* ssid_office  = "YOUR_OFFICE_SSID";
+const char* pass_office  = "YOUR_OFFICE_PASSWORD";
+
+const char* ssid_hotspot = "YOUR_HOTSPOT_SSID";
+const char* pass_hotspot = "YOUR_HOTSPOT_PASSWORD";
+
+#endif
