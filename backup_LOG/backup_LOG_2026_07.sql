68ce8431e866a63384e6f177a17da72f187881f2
Marchino1978 <marco.brambill@gmail.com>
2026-08-28 12:18:23 +0200

    fix

--- backup_LOG/backup_LOG_2026_07.sql
+++
@@ -1,4 +0,0 @@
--- BACKUP MENSILE LOGS DOM-US PERIODO: 2026-07-01T00:00:00Z - 2026-07-31T23:59:59Z
-
--- TABELLA: logs
-INSERT INTO logs (id, created_at, color, message) VALUES (3, '2026-07-28 10:24:40', 'red', 'alarm');
