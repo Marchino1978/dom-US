795fcae028b56f9ba27919c6ab831127d37e0127
Marchino1978 <marco.brambill@gmail.com>
2026-09-01 10:58:02 +0200

    fix

---
+++ backup_LOG/backup_LOG_2026_08.sql
@@ -0,0 +1,6 @@
+-- BACKUP MENSILE LOGS DOM-US PERIODO: 2026-08-01T00:00:00Z - 2026-08-31T23:59:59Z
+
+-- TABELLA: logs
+INSERT INTO logs (id, created_at, color, message) VALUES (22, '2026-08-31 23:05:18', '🔴', 'ALARM - TEST INTRUSION SCRIPT');
+INSERT INTO logs (id, created_at, color, message) VALUES (23, '2026-08-31 23:08:43', '🔴', 'ALARM - TEST INTRUSION SCRIPT');
+INSERT INTO logs (id, created_at, color, message) VALUES (24, '2026-08-31 23:09:38', '🔴', 'ALARM - TEST INTRUSION SCRIPT');
